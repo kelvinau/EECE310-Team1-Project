2a1f3cfa1422c48e2bc554c3a881a1c0910646f2
kelvinau <kelvinau4413@hotmail.com>
2014-03-28 02:19:54 -0700

    Reformatted json string for the data retrieved

--- db.php
+++ db.php
@@ -42,7 +42,37 @@ function result_search($key){
 	while($row)
 	  {
 
-	  $json .= '{"Course_Subject":"'.$row['Course_Subject'].'", "Course_No":"'.$row['Course_No'].'", "Course_Section":"'.$row['Course_Section'].'", "Title":"'.$row['Title'].'", "Professor":"'.$row['Professor'].'", "Enrolled":"'.$row['Enrolled'].'", "Avg":"'.$row['Avg'].'", "Std_dev":"'.$row['Std_dev'].'", "Highest":"'.$row['Highest'].'", "Lowest":"'.$row['Lowest'].'", "Pass":"'.$row['Pass'].'", "Fail":"'.$row['Fail'].'", "Withdraw":"'.$row['Withdraw'].'", "Audit":"'.$row['Audit'].'", "Other":"'.$row['Other'].'", "Marks0to9":"'.$row['Marks0to9'].'", "Marks10to19":"'.$row['Marks10to19'].'", "Marks20to29":"'.$row['Marks20to29'].'", "Marks30to39":"'.$row['Marks30to39'].'", "Marks40to49":"'.$row['Marks40to49'].'", "MarksLessThan50":"'.$row['MarksLessThan50'].'", "Marks50to54":"'.$row['Marks50to54'].'", "Marks55to59":"'.$row['Marks55to59'].'", "Marks60to63":"'.$row['Marks60to63'].'", "Marks64to67":"'.$row['Marks64to67'].'", "Marks68to71":"'.$row['Marks68to71'].'", "Marks72to75":"'.$row['Marks72to75'].'", "Marks76to79":"'.$row['Marks76to79'].'", "Marks80to84":"'.$row['Marks80to84'].'", "Marks85to89":"'.$row['Marks85to89'].'", "Marks90to100":"'.$row['Marks90to100'].'"}';
+	  $json .= '{"Course_Subject":"'.$row['Course_Subject']
+	  .'", "Course_No":"'.$row['Course_No']
+	  .'", "Course_Section":"'.$row['Course_Section']
+	  .'", "Title":"'.$row['Title']
+	  .'", "Professor":"'.$row['Professor']
+	  .'", "Enrolled":"'.$row['Enrolled']
+	  .'", "Avg":"'.$row['Avg']
+	  .'", "Std_dev":"'.$row['Std_dev']
+	  .'", "Highest":"'.$row['Highest']
+	  .'", "Lowest":"'.$row['Lowest']
+	  .'", "Pass":"'.$row['Pass']
+	  .'", "Fail":"'.$row['Fail']
+	  .'", "Withdraw":"'.$row['Withdraw']
+	  .'", "Audit":"'.$row['Audit']
+	  .'", "Other":"'.$row['Other']
+	  .'", "Marks0to9":"'.$row['Marks0to9']
+	  .'", "Marks10to19":"'.$row['Marks10to19']
+	  .'", "Marks20to29":"'.$row['Marks20to29']
+	  .'", "Marks30to39":"'.$row['Marks30to39']
+	  .'", "Marks40to49":"'.$row['Marks40to49']
+	  .'", "MarksLessThan50":"'.$row['MarksLessThan50']
+	  .'", "Marks50to54":"'.$row['Marks50to54']
+	  .'", "Marks55to59":"'.$row['Marks55to59']
+	  .'", "Marks60to63":"'.$row['Marks60to63']
+	  .'", "Marks64to67":"'.$row['Marks64to67']
+	  .'", "Marks68to71":"'.$row['Marks68to71']
+	  .'", "Marks72to75":"'.$row['Marks72to75']
+	  .'", "Marks76to79":"'.$row['Marks76to79']
+	  .'", "Marks80to84":"'.$row['Marks80to84']
+	  .'", "Marks85to89":"'.$row['Marks85to89']
+	  .'", "Marks90to100":"'.$row['Marks90to100'].'"}';
 	  
 	  if ($row = mysqli_fetch_array($result))
 	  	$json .= ','; 
